c3a8826e715ebdf71951f439f932a7ea71d4895e
snaxy318 <suryanshtejas@gmail.com>
2024-07-16 00:25:23 +0530

    added gradient_container

---
+++ lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  GradientContainer(this.colors,{super.key});
+  final List<Color> colors;
+  final start = Alignment.topLeft;
+  final end = Alignment.bottomRight;
+
+
+  @override
+  Widget build (context) {
+    return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors:colors,
+              begin: start,
+              end: end
+            )
+          ),
+        );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
+import './gradient_container.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors:[
+        body: GradientContainer(const [
                 Color.fromARGB(255, 215, 44, 245),
                 Color.fromARGB(255, 108, 9, 125),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            )
-          ),
-        ),
+              ]),
       ),
     ),
   );
