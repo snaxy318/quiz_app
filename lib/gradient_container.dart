944646ce443ff5c29df125bf3e42e4a8e6f9d344
snaxy318 <suryanshtejas@gmail.com>
2024-07-16 22:37:00 +0530

    added text and button on start page

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.colors,{super.key});
+  GradientContainer(this.colors, {super.key});
   final List<Color> colors;
   final start = Alignment.topLeft;
   final end = Alignment.bottomRight;
 
-
-  @override
-  Widget build (context) {
-    return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors:colors,
-              begin: start,
-              end: end
-            )
-          ),
-          child: Center(child: Image.asset('assets/images/quiz-logo.png',width: 300,)),
-        );
+  void startButton() {
+    //...
   }
 
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: start,
+          end: end,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 250,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: startButton,
+              child: const Text(
+                'Start quiz',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
