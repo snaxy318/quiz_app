97f1ec9bd7bebd6439fd849caae35fee9edae7e5
snaxy318 <suryanshtejas@gmail.com>
2024-07-27 21:59:04 +0530

    rendered content conditionally for showing question_screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
 import 'home_page.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
-  @override 
-  State<Quiz> createState(){
+  @override
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
+  Widget currentScreen = const HomePage();
+
+  void switchScreen() {
+    setState(() {
+      currentScreen = const QuestionPage();
+    });
+  }
+
   @override
-  Widget build(context){
+  Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration:const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
                 Color.fromARGB(255, 215, 44, 245),
                 Color.fromARGB(255, 108, 9, 125),
               ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),),
-          child: const HomePage(),),
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: currentScreen,
+        ),
       ),
     );
   }
